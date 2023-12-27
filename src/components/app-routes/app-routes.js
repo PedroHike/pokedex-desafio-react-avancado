74e63a345c646977c43a3b395111d6df61f7e419
Pedro Henrique Amaral <ph.amaral1991@gmail.com>
2023-12-27 20:26:26 -0300

    ajuste em rotas

--- src/components/app-routes/app-routes.js
+++ src/components/app-routes/app-routes.js
@@ -3,7 +3,7 @@ import { HomePage } from "../body"
 import { Pokemon } from "../pokemon"
 
 export const AppRoutes = () => (
-    <BrowserRouter>
+    <BrowserRouter basename="/desafio-react-avancado">
         <Routes>
             <Route exact path='/' element={<HomePage/>}/>
             <Route exact path='/pokemon/:id' element={<Pokemon/>}/>
