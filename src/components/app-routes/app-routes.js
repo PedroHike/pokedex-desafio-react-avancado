83c81155bb2eb1813becf309756ecaae49f182ee
pedro henrique amaral <ph.amaral1991@gmail.com>
2023-12-29 13:52:01 -0300

    ajustes de codigo

--- src/components/app-routes/app-routes.js
+++ src/components/app-routes/app-routes.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { HomePage } from "../body"
+import { HomePage } from "../home"
 import { Pokemon } from "../pokemon"
+import { PokemonDetails } from "../pokemon-details"
 
 export const AppRoutes = () => (
     <BrowserRouter basename="/desafio-react-avancado">
         <Routes>
             <Route exact path='/' element={<HomePage/>}/>
-            <Route exact path='/pokemon/:id' element={<Pokemon/>}/>
+            <Route exact path='/pokemon/:id' element={<PokemonDetails/>}/>
         </Routes>
     </BrowserRouter>
 )
